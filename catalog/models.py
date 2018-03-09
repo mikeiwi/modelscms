edabda91b15ef2126f49f6bee0265e405e2714ca
mikeiwi <michael.valenciav@gmail.com>
2018-03-09 14:20:53 -0500

    catalog app added, with model and photo models.

---
+++ catalog/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+class ModelPerson(models.Model):
+    """Represents a person in the catalog."""
+    first_name = models.CharField(max_length=100, blank=True)
+    last_name = models.CharField(max_length=100, blank=True)
+    nickname = models.CharField(max_length=100, blank=True)
+
+
+class Photo(models.Model):
+    """An element o a Model's gallery."""
+    location = models.CharField('URL de archivo', max_length=255,
+                                null=True, blank=True)
+    model_person = models.ForeignKey('catalog.ModelPerson',
+                                     on_delete=models.CASCADE)
