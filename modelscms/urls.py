bc6f359f80abd29d08fba4898e295a435e315890
mikeiwi <michael.valenciav@gmail.com>
2018-03-25 11:27:25 -0500

    home init.

--- modelscms/urls.py
+++ modelscms/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
+    path('', include('catalog.urls')),
     path('admin/', admin.site.urls),
     path(r's3direct/', include('s3direct.urls')),
 ]
