0c4ad9dea6bd248f2b72c883eeff0898aec18bf5
mikeiwi <michael.valenciav@gmail.com>
2018-03-25 15:25:41 -0500

    profile page loading.

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import home
+from .views import home, profile
 
 
 urlpatterns = [
     path('', home),
+    path('model/<nickname>', profile, name='model-profile'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import ModelPerson
 
@@ -6,3 +6,8 @@ from .models import ModelPerson
 def home(request):
     models = ModelPerson.objects.all()
     return render(request, 'catalog/home.html', {'models': models})
+
+
+def profile(request, nickname):
+    model = get_object_or_404(ModelPerson, nickname=nickname)
+    return render(request, 'catalog/profile.html', {'model': model})
